c8a049d5838622e8623c146f35d4c7875e239100
J4N <142495328+J4NBI@users.noreply.github.com>
2025-01-17 15:47:53 +0100

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from '@/sections/Tape'
-import { EditsSection } from "@/sections/Edits";
+import { TestimonialsSection } from "@/sections/Testimonials";
 import { AboutSection } from "@/sections/About";
 import { ContactSection } from "@/sections/Contact";
 import { Footer } from "@/sections/Footer";
@@ -15,7 +15,7 @@ export default function Home() {
      <HeroSection />
      <ProjectsSection />
      <TapeSection />
-     <EditsSection />
+     <TestimonialsSection />
      <AboutSection />
      <ContactSection />
      <ContactForm />   
